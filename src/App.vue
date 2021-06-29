40f48bbe7aabcc441092f5d310770fa216f7e0c0
ASUS <2384851728@qq.com>
2021-06-29 17:18:15 +0800

    '初始化'

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <Tar>
+    <tarBarItem path="/home">
+      <img slot="item-icon" src="./assets/img/tabbar/home.svg">
+      <img slot="item-icon-active" src="./assets/img/tabbar/home-active.svg">
+      <div slot="item-text">首页</div>
+    </tarBarItem>
+    <tarBarItem path="/category">
+      <img slot="item-icon" src="./assets/img/tabbar/category.svg">
+      <img slot="item-icon-active" src="./assets/img/tabbar/category-active.svg">
+      <div slot="item-text">分类</div>
+    </tarBarItem>
+    <tarBarItem path="/cart">
+      <img slot="item-icon" src="./assets/img/tabbar/cart.svg">
+      <img slot="item-icon-active" src="./assets/img/tabbar/cart-active.svg">
+      <div slot="item-text">购物车</div>
+    </tarBarItem>
+    <tarBarItem path="/Profile">
+      <img slot="item-icon" src="./assets/img/tabbar/mine.svg">
+      <img slot="item-icon-active" src="./assets/img/tabbar/mine-active.svg">
+      <div slot="item-text">我的</div>
+    </tarBarItem>
+  </Tar>
+  </div>
+</template>
+
+<script>
+  import Tar from './components/tabber/tabber'
+  import tarBarItem from './components/tabber/tarBarItem'
+export default {
+  name: 'App',
+  components: {
+    Tar,
+    tarBarItem
+  }
+}
+</script>
+
+<style>
+  @import "./assets/css/base.css";
+
+</style>
---
+++ src/components/tabber/tarBarItem.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="tar-item" @click="itemClick">
+      <div v-if="!isActive"><slot name="item-icon"></slot></div>
+      <div v-else><slot name="item-icon-active"></slot></div>
+      <div :class="{active:isActive}"><slot name="item-text"></slot></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "tarBarItem",
+  props:{
+    path:String
+  },
+  data(){
+    return {
+      // isActive:true
+     }
+  },
+  computed:{
+    isActive(){
+      return this.$route.path.indexOf(this.path) !==-1
+    }
+  },
+  methods:{
+    itemClick(){
+      this.$router.replace(this.path)
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+.tar-item{
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+.tar-item img{
+  width: 24px;
+  height: 24px;
+  padding-top: 4px;
+  vertical-align: middle;
+  margin-bottom: 3px;
+}
+.active {
+  color: mediumvioletred;
+}
+</style>
